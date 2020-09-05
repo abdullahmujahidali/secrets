4d9c1d01d22def3a323c07e0176c58ef083c3414
Abdullah Mujahid  ◘_◘ <48271080+abdullahmujahidali@users.noreply.github.com>
2020-09-05 14:13:55 +0500

    Added Environment Vars
    Security level 3

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //jshint esversion:6
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
 
 const app = express();
+
+console.log(process.env.API_KEY);
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs')
@@ -18,9 +22,9 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisalittlesecret.";
 
-userSchema.plugin(encrypt, { secret: secret , encryptedFields:["password"] });
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields:["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
